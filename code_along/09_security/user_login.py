ac9cfbfb38490969e3a48df46ac0887d4f651688
John Sandsjö <john.sandsjo@gmail.com>
2025-03-25 10:36:35 +0100

    security

---
+++ code_along/09_security/user_login.py
@@ -0,0 +1,18 @@
+from connect_postgres import connect_postgres
+from sqlalchemy import text
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+with connect_postgres().connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}';")).fetchall()
+    
+    if result: #exploit truthy here with a filled vs not tuple
+        print("login successful")
+        print("you can eat all the icecream you want")
+    else:
+        print("another failure")
+
+# bob' OR '1' ='1 
+# ' OR '1' = '1' --
+# '; DROP TABLE users; --
